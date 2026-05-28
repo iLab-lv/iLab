0cb00e4eaab2ee6960719fc54983df0b124aec5c
iLab <info@ilab.lv>
2026-05-28 13:23:51 -0700

    script cleanup

--- app/(site)/(info)/pieraksties-remontam/paldies/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function PaldiesPage() {
-  return (
-    <main className="container" style={{ padding: '2rem 0' }}>
-      <h1>Paldies!</h1>
-      <p>Jūsu pieteikums ir saņemts. Drīzumā ar jums sazināsimies.</p>
-    </main>
-  );
-}
