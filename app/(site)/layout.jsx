7cecaea80da5f2aafa2e2c8ec68e9d34fa0298c3
iLab <info@ilab.lv>
2026-06-05 15:26:26 -0700

    Remove direct GA and defer site widgets

--- app/(site)/layout.jsx
+++ app/(site)/layout.jsx
@@ -5,11 +5,10 @@ import { getSiteSettings } from '@/lib/siteSettings';
 import StructuredData from './StructuredData';
 import NavBar from './ui/navbar/NavBar';
 import Controls from './ui/controls/Controls';
-import BottomBar from './ui/bottombar/BottomBar';
+import DeferredBottomBar from './ui/bottombar/DeferredBottomBar';
 import Footer from './ui/footer/Footer';
 import { UiDialogsProvider } from './ui/providers/UiDialogsProvider';
-import CookieConsent from './ui/cookie-consent/CookieConsent';
-import ConsentAnalytics from './ui/cookie-consent/ConsentAnalytics';
+import DeferredCookieConsent from './ui/cookie-consent/DeferredCookieConsent';
 
 import l from './Layout.module.scss';
 
@@ -34,7 +33,6 @@ export default async function SiteLayout({ children }) {
   return (
     <div className={l.siteRoot}>
       <StructuredData siteSettings={siteSettings} />
-      <ConsentAnalytics />
 
       <UiDialogsProvider locale={locale} siteSettings={siteSettings}>
         <a
@@ -61,13 +59,13 @@ export default async function SiteLayout({ children }) {
           />
         </div>
 
-        <BottomBar socials={socials} />
+        <DeferredBottomBar socials={socials} />
 
         <main id="main">{children}</main>
 
         <Footer locale={locale} siteSettings={siteSettings} />
 
-        <CookieConsent locale={locale} />
+        <DeferredCookieConsent locale={locale} />
       </UiDialogsProvider>
     </div>
   );
--- app/layout.jsx
+++ app/layout.jsx
@@ -111,7 +111,7 @@ export default async function RootLayout({ children }) {
 
         {children}
 
-        <Script id="gtm-base" strategy="afterInteractive">
+        <Script id="gtm-base" strategy="lazyOnload">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
